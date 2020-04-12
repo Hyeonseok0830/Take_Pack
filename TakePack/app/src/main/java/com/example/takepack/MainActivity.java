b44b3b11979166ab9467cae6fe79541ce9c82de8
Hyeonseok0830 <96hyeonseok@gmail.com>
2020-04-12 16:49:59 +0900

    TakePack Update
    위치추가 팝업 생성
    EditText 입력 후 키보드 내리기
    다음 단계 : 리스트 뷰에 아이템 추가하기

--- TakePack/app/src/main/java/com/example/takepack/MainActivity.java
+++ TakePack/app/src/main/java/com/example/takepack/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.takepack;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +28,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private FragmentManager fragmentManager;
     private MapFragment mapFragment;
 
+    private String title;
+    private Button hidebtn;
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +45,58 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
         mapFragment.getMapAsync(this);
 
+
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng location = new LatLng(35.229681, 128.577923); //우리집 위치
+    public void onMapReady(final GoogleMap googleMap) {
+        googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(final LatLng point) {
+                final Dialog ad = new Dialog(MainActivity.this); // 다이얼로그 객체 생성
+                ad.setTitle("다이얼로그의 제목");
+                ad.setContentView(R.layout.popup_activity); // 다이얼로그 화면 등록
+                Button pb = (Button) ad.findViewById(R.id.check);
+                Button nb = (Button) ad.findViewById(R.id.cancel);
+
+                Button kh = (Button) ad.findViewById(R.id.keyhide);
+
+                final EditText et = (EditText) ad.findViewById(R.id.edtext);
+                kh.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+
+                        imm.hideSoftInputFromWindow(et .getWindowToken(), 0); //숨기기
+
+                    }
+                });
+
+                pb.setOnClickListener(new View.OnClickListener() { // 확인버튼
+                    @Override
+                    public void onClick(View v) {
+                        title = et.getText().toString();
+                        MarkerOptions mOption = new MarkerOptions();
+                        mOption.title(title);
+                        Double latitude = point.latitude; // 위도
+                        Double longitude = point.longitude; // 경도
+                        mOption.snippet("엄청이뻐 ㅋ");
+                        mOption.position(new LatLng(latitude, longitude));
+                        googleMap.addMarker(mOption);
+                        ad.dismiss();
+                    }
+                });
+                nb.setOnClickListener(new View.OnClickListener() { //취소버튼
+                    @Override
+                    public void onClick(View v) {
+                        ad.dismiss();
+                    }
+                });
+                ad.show();
+
+            }
+        });
+        LatLng location = new LatLng(36.892498, 126.628380); //우리집 위치
         MarkerOptions markerOptions=new MarkerOptions();
         markerOptions.title("우리집");
         markerOptions.snippet("스니펫");
