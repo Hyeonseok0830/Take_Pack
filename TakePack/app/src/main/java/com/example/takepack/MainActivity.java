34bdafbf08654bd228b3d40dd56cc579a835f287
Hyeonseok0830 <96hyeonseok@gmail.com>
2020-04-12 13:29:48 +0900

    Git연동 완료
    Git연동 후 Google Map API KEY 생성 셋팅

--- TakePack/app/src/main/java/com/example/takepack/MainActivity.java
+++ TakePack/app/src/main/java/com/example/takepack/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.takepack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager=getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(35.229681, 128.577923); //우리집 위치
+        MarkerOptions markerOptions=new MarkerOptions();
+        markerOptions.title("우리집");
+        markerOptions.snippet("스니펫");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+
+
     }
 }
